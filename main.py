8a754c5b08ced636ae634a9367bd6dd5e0eaf4f1
Sultan Saladdin <sultansaladd@gmail.com>
2021-01-05 15:27:02 +0700

    add class Snack

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 class Snack:
-    def __init__(self):
-        print('Hello')
+    def __init__(self, inputSnack, inputHarga):
+        self.nama = inputSnack
+        self.harga = inputHarga
 
-print(Snack.__init__)
+Snack1 = Snack("Biskuit", 6000)
+Snack2 = Snack("Chips", 8000)
+Snack3 = Snack("Oreo", 10000)
+Snack4 = Snack("Tango", 12000)
+Snack5 = Snack("Cokelat", 15000)
+
+
+
+# class Uang:
+#     def __init__(self, inputUang):
+#         self.uang = inputUang
+
+# Uang
+
