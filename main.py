409a8bb552707d4e6fa18c9b5721ae442a44a255
Sultan Saladdin <sultansaladd@gmail.com>
2021-01-06 02:27:02 +0700

    Feature buy_snack is done

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 class Snack:
-    def __init__(self, inputSnack, inputHarga):
-        self.nama = inputSnack
-        self.harga = inputHarga
+    snack_dict = {'biskuit': 6000, 'chips': 8000, 'oreo':10000, "tango": 12000, 'cokelat': 15000}
+    stock_dict = {'biskuit': 5, 'chips': 4, 'oreo': 7, "tango": 6, 'cokelat': 1}
+    money = [2000, 5000, 10000, 20000, 50000]
 
-Snack1 = Snack("Biskuit", 6000)
-Snack2 = Snack("Chips", 8000)
-Snack3 = Snack("Oreo", 10000)
-Snack4 = Snack("Tango", 12000)
-Snack5 = Snack("Cokelat", 15000)
+    def __init__(self, snack_name, payment, total_item):
+        self.name = snack_name
+        self.price = payment
+        self.total = total_item
+    
+    def show(self):
+        for key, value in Snack.stock_dict.items():
+            if key == self.name and value > 0:
+                for key, value in Snack.snack_dict.items():
+                    if self.price in Snack.money:
+                        changes = self.price - value
+                        if key == self.name:
+                            # break
+                            print(f"You buy a {self.name} with {self.price}\nChanges: {changes}")
+            elif key == self.name and value < 0:
+                print(f"{self.name} is empty")
+                
+                                
+        if not self.price in Snack.money:
+            print("Vending Machine cannot accept these type of money")
+           
 
-
-
-# class Uang:
-#     def __init__(self, inputUang):
-#         self.uang = inputUang
-
-# Uang
-
+buy_snack = Snack("biskuit", 20000, 1)
+buy_snack.show()
